29bafd007ee00258f985c833c623d22867e414c4
francoling <francoling@tencent.com>
2024-06-28 15:49:13 +0800

    31. 认识观察者模式 KVO 并展示页面加载进度

--- SampleApp/GTDetailViewController.m
+++ SampleApp/GTDetailViewController.m
@@ -11,10 +11,16 @@
 @interface GTDetailViewController () <WKNavigationDelegate>
 
 @property (nonatomic, strong, readwrite) WKWebView *webView;
+@property (nonatomic, strong, readwrite) UIProgressView *progressView;
 
 @end
 
 @implementation GTDetailViewController
+
+- (void)dealloc
+{
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,7 +31,14 @@
         self.webView;
     })];
     
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, 20)];
+        self.progressView;
+    })];
+    
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.bing.com"]]];
+    
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
@@ -38,4 +51,7 @@
     NSLog(@"");
 }
 
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey, id> *)change context:(nullable void *)context {
+    self.progressView.progress = self.webView.estimatedProgress;
+}
 @end
